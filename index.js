014d4d1d752dbad831cc5cbf6f1145ec140dce36
andyzen619 <andyzen619@gmail.com>
2019-10-18 17:39:18 +0000

    Added Sum from a to b functionality

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-
+/**
+ * Sum all the values from fromN up to toN
+ */
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  //Base case: Returns the last number to sum.
+  if (fromN == toN) {
+    return toN;
+  }
+  //Inductive step: Adds current number to the sum that is done in the next iteration.
+  else {
+    return fromN + sum(fromN + 1, toN);
+  }
 }
 
 module.exports = sum;
